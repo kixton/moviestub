96ded928d03e675fdad654d7d7b73b18cbfd8d08
Kim Pham <ms.kimpham@gmail.com>
2014-11-27 22:31:03 -0600

    created factory to get movie data resource

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express"),
-    fs = require('fs'),
+    fs = require("fs"),
     port = process.env.PORT || 2595,
-    morgan = require('morgan'),
-    bodyParser = require('body-parser');
+    morgan = require("morgan"),
+    bodyParser = require("body-parser");
 
 // create express app
 var app = express();
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -17,12 +17,17 @@ app.set("view options", {
   layout: false
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
  
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   // render result in index.html
-  res.render('public/index.html');
+  res.render("public/index.html");
+});
+
+app.get("/movies", function (req, res) {
+  var movies = require("./data/movies.json");
+  res.json(movies);
 });
  
 app.listen(port);
-console.log('Express server running at http://localhost:' + port);
+console.log("Express server running at http://localhost:" + port);
